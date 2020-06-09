7f0f0db2a1bdb447ae48129079bb0b1071b4972f
TKgasa <tumelo.kgasa@gmail.com>
2020-06-09 22:04:29 +0200

    Intial commmit

---
+++ app/src/main/java/com/example/taxi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.taxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button nDriver, nCustomer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nDriver = (Button) findViewById(R.id.driver_btn);
+        nCustomer = (Button) findViewById(R.id.customer_btn);
+
+
+        // OnClick listener for the driver nad customers buttons
+
+        nDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
+        nCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
